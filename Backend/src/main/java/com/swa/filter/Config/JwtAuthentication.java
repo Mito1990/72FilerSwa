5e2273950a6408ed837bb349974e4185e96dacd1
Michael Toetsches <mitoit00@hs-esslingen.de>
2023-05-03 01:56:33 +0200

    feat: add JwtService

--- Backend/src/main/java/com/swa/filter/Config/JwtAuthentication.java
+++ Backend/src/main/java/com/swa/filter/Config/JwtAuthentication.java
@@ -3,6 +3,9 @@ package com.swa.filter.Config;
 import java.io.IOException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.swa.filter.Services.JwtService;
+
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,11 +16,20 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class JwtAuthentication extends OncePerRequestFilter{
 
+    private final JwtService jwtService;
     @Override
     protected void doFilterInternal(
         HttpServletRequest request, 
         HttpServletResponse response, 
         FilterChain filterChain)throws ServletException, IOException{
-
+        final String authHeader = request.getHeader("Authorization");
+        final String jwtToken;
+        final String username;
+        if(authHeader==null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwtToken = authHeader.substring(7);
+        username = jwtToken.extractUsername(jwtToken);
     }
 }
