32a6813b1a695a6c283905b7ac4f04bf3d045f5a
Michael Toetsches <mitoit00@hs-esslingen.de>
2023-05-03 10:26:36 +0200

    fix: add functions to JwtAuthentication

--- Backend/src/main/java/com/swa/filter/Config/JwtAuthentication.java
+++ Backend/src/main/java/com/swa/filter/Config/JwtAuthentication.java
@@ -2,9 +2,12 @@ package com.swa.filter.Config;
 
 import java.io.IOException;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -38,6 +41,12 @@ public class JwtAuthentication extends OncePerRequestFilter{
         username = jwtService.extractUsername(jwtToken);
         if( username != null && SecurityContextHolder.getContext().getAuthentication() == null){
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+            if(jwtService.isTokenValid(jwtToken, userDetails)){
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
         }
+        filterChain.doFilter(request, response);
     }
 }
