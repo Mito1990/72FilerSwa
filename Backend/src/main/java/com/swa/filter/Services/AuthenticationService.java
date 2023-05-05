2d53834b779cc9d809ea422b59e41772d106d5cc
Michael Toetsches <mitoit00@hs-esslingen.de>
2023-05-05 17:08:37 +0200

    feat: add FileService, change some UserController and AUthenticationService

--- Backend/src/main/java/com/swa/filter/Services/AuthenticationService.java
+++ Backend/src/main/java/com/swa/filter/Services/AuthenticationService.java
@@ -21,22 +21,28 @@ public class AuthenticationService {
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
     private final UserService userService;
-        
+    private final FileService fileService;
+            
     
     public AuthenticationResponse register(RegisterRequest registerRequest) {
-        var user = User.builder()
-                       .name(registerRequest.getName())
-                       .username(registerRequest.getUsername())
-                       .password(passwordEncoder.encode(registerRequest.getPassword()))
-                       .role(Role.USER)
-                       .build();
-        if(userService.checkIfUserExists(registerRequest.getUsername())==false){
-            userRepository.save(user);
-            var jwtToken = jwtService.generateToken(user);
-            return AuthenticationResponse.builder().message(jwtToken).build();
+        if(userService.checkIfUserExists(registerRequest.getUsername())){
+            return AuthenticationResponse.builder().message("User Exits already!").build();
         }
-        else return AuthenticationResponse.builder().message("User Exits already!").build();
+        else{
+            var user = User.builder()
+                        .name(registerRequest.getName())
+                        .username(registerRequest.getUsername())
+                        .password(passwordEncoder.encode(registerRequest.getPassword()))
+                        .role(Role.USER)
+                        .build();
+            userRepository.save(user);
+            fileService.createPathAndFile(registerRequest.getUsername(), user.getUser_id());
+            System.out.println(user.getUser_id());
+            // var jwtToken = jwtService.generateToken(user);
+            return AuthenticationResponse.builder().message("User succsesfull registered!").build();
+        }
     }
+    
 
     public AuthenticationResponse authenticate(AuthenticationRequest authenticationRequest) {
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword()));
