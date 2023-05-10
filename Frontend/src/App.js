94337fd7b7ba8d523eb93df4d0982f4e899a7d46
phscit05 <phscit05@hs-esslingen.de>
2023-05-10 17:36:37 +0200

    Created Navbar

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,24 +1,30 @@
 import React from "react";
+import Navbar from './Components/Navbar/Navbar';
 import {BrowserRouter,Routes,Route,Navigate} from "react-router-dom";
-import { Register } from "./Components/Register"; 
-import { Login } from "./Components/Login"; 
-import { Home } from "./Components/Home"; 
+import { SignUp } from "./Components/pages/signup"; 
+import { Login } from "./Components/pages/login"; 
+import Home from './Components/pages/home';
+import About from './Components/pages/about';
+import Filebase from './Components/pages/filebase';
+import Teams from './Components/pages/teams';
 
 export const App = () =>{
 	
 return (
 	<>
 	<BrowserRouter>
+	<Navbar />
 	<Routes>
-		<Route path="/" element={<Navigate replace to="/Login" />} />	
-		<Route exact path="/Login" element={<Login/>}/>
-		<Route exact path="/Register" element={<Register/>}/>
 		<Route exact path="/Home" element={<Home/>}/>
-	</Routes>
+		<Route exact path="/Teams" element={<Teams/>}/>
+		<Route exact path="/Filebase" element={<Filebase/>}/>
+		<Route exact path="/About" element={<About/>}/>
+		<Route exact path="/Login" element={<Login/>}/>
+		<Route exact path="/SignUp" element={<SignUp/>}/>
+ 	</Routes>
 	</BrowserRouter>
 	</>
 );
 }
 
 export default App;
-
